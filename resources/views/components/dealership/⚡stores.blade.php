12ec714ace63ab9de391e6470f431363f29a8659
jlarm <117611704+jlarm@users.noreply.github.com>
2026-01-26 13:17:34 -0600

    Delete store

--- resources/views/components/dealership/⚡stores.blade.php
+++ resources/views/components/dealership/⚡stores.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Dealership;
+use App\Models\Store;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\On;
 use Livewire\Component;
@@ -10,11 +11,26 @@ new class extends Component {
 
     #[Computed]
     #[On('store-created')]
+    #[On('store-deleted')]
     public function stores()
     {
         return $this->dealership
             ->stores()
             ->get();
+    }
+
+    public function delete(int $storeId): void
+    {
+        $store = Store::findOrFail($storeId);
+
+        $store->delete();
+
+        Flux::toast(
+            text: 'Store deleted successfully',
+            variant: 'success',
+        );
+
+        $this->dispatch('store-deleted');
     }
 };
 ?>
@@ -22,7 +38,19 @@ new class extends Component {
 <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
     @forelse($this->stores as $store)
         <flux:card class="hover:bg-slate-50 transition">
-            <flux:heading>{{ $store->name }}</flux:heading>
+            <div class="flex items-center justify-between">
+                <flux:heading>{{ $store->name }}</flux:heading>
+                <flux:dropdown position="bottom" align="end">
+                    <flux:button variant="ghost" size="xs" icon="ellipsis-vertical" inset="top right bottom"/>
+
+                    <flux:navmenu>
+                        <flux:navmenu.item href="#" icon="pencil">Edit</flux:navmenu.item>
+                        <flux:navmenu.item wire:confirm="Are you sure you want to delete this contact?"
+                                           wire:click="delete({{ $store->id }})" icon="trash" variant="danger">Delete
+                        </flux:navmenu.item>
+                    </flux:navmenu>
+                </flux:dropdown>
+            </div>
             <flux:text class="mt-1">
                 {{ $store->address }}<br/>
                 {{ $store->city }}, {{ $store->state }} {{ $store->zip_code }}
